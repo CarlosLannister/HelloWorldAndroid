ca063e7b0d35ad24288a29eaa1faf000e8ac2502
carlos <carlos.cilleruelo@uah.es>
2021-01-23 13:18:02 +0100

    Added kotlin-android-extensions

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,19 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.text_view) as TextView
-        val button: Button = findViewById<Button>(R.id.button)
-
         var number = 0
         button.setOnClickListener  {
             number++
-            textView.text = number.toString()
+            text_view.text = number.toString()
         }
     }
 }
